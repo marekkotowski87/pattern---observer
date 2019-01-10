50a85ea4567eb9e4a5a706f92982fba8902b88ee
Marek Kotowski <marek.kotowski87@gmail.com>
2019-01-10 22:54:20 +0100

    Initial commit

---
+++ app6.js
@@ -0,0 +1,57 @@
+class EventObserver {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+    console.log(`You are now subscribed to ${fn.name}`);
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter(function(item) {
+      if (item != fn) {
+        return item;
+      }
+    });
+    console.log(`You are now unsubscribed from ${fn.name}`);
+  }
+
+  fire() {
+    this.observers.forEach(function(item) {
+      item.call();
+    });
+  }
+}
+
+const click = new EventObserver();
+
+// Event listeners
+document.querySelector('.sub-ms').addEventListener('click', function() {
+  click.subscribe(getCurMiliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+  click.unsubscribe(getCurMiliseconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function() {
+  click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function() {
+  click.unsubscribe(getCurSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function() {
+  click.fire();
+});
+
+// Click handler
+const getCurMiliseconds = function() {
+  console.log(`Current Miliseconds: ${new Date().getMilliseconds()}`);
+};
+
+const getCurSeconds = function() {
+  console.log(`Current Seconds: ${new Date().getSeconds()}`);
+};
